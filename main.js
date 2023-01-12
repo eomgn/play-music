5d846731929d936c6773457466474ec0077e44d8
MATHEUS  GABRIEL NOGUEIRA <matheusnogueirabrisa@grupobrisanet.onmicrosoft.com>
2023-01-12 16:01:57 -0300

    actualizing input range

--- main.js
+++ main.js
@@ -1,75 +1,76 @@
-let qs = el => document.querySelector(el)
+let qs = (el) => document.querySelector(el);
 
-let durationInitial = qs('.durationInitial')
-let durationTotal = qs('.durationTotal')
-let range = qs('#range')
+let durationInitial = qs(".durationInitial");
+let durationTotal = qs(".durationTotal");
+let range = qs("#range");
 
-let timerLeft = 200
-let timerRight = 0
-let intervalLeft
-let intervalRight
-let minutes
-let seconds
+let timerLeft = 200;
+let timerRight = 0;
+let intervalLeft;
+let intervalRight;
+let minutes;
+let seconds;
 
-range.max = timerLeft
+range.max = timerLeft;
+
+function inputRange() {
+  range.value = timerRight;
+
+  let xd = 100 / timerLeft;
+
+  console.log(xd);
+}
 
 function changingValuesLeft() {
-   minutes = parseInt(timerLeft / 60)
-   seconds = parseInt(timerLeft % 60)
+  minutes = parseInt(timerLeft / 60);
+  seconds = parseInt(timerLeft % 60);
 
-   timerLeft > 0 ? timerLeft-- : timerLeft--
+  timerLeft > 0 ? timerLeft-- : timerLeft--;
 
-   minutes = minutes < 10 ? '0' + minutes : minutes
-   seconds = seconds < 10 ? '0' + seconds : seconds
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
 
-   durationInitial.innerHTML = `${minutes}:${seconds}`
+  durationInitial.innerHTML = `<p>${minutes}</p><p>:</p><p>${seconds}</p>`;
 }
 
 function changingValuesRight() {
-   minutes = parseInt(timerRight / 60)
-   seconds = parseInt(timerRight % 60)
+  minutes = parseInt(timerRight / 60);
+  seconds = parseInt(timerRight % 60);
 
-   timerRight > 0 ? timerRight++ : timerRight++
+  timerRight > 0 ? timerRight++ : timerRight++;
 
-   minutes = minutes < 10 ? '0' + minutes : minutes
-   seconds = seconds < 10 ? '0' + seconds : seconds
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
 
-   durationTotal.innerHTML = `${minutes}:${seconds}`
-
-   range.value = timerRight
+  durationTotal.innerHTML = `<p>${minutes}</p><p>:</p><p>${seconds}</p>`;
+  inputRange();
 }
 
 function start() {
-   intervalLeft = setInterval(() => {
-      if (timerLeft < 0) {
-         clearInterval(intervalLeft)
-      } else {
-         changingValuesLeft()
-      }
-   }, 100)
+  intervalLeft = setInterval(() => {
+    if (timerLeft < 0) {
+      clearInterval(intervalLeft);
+    } else {
+      changingValuesLeft();
+    }
+  }, 100);
 
-   intervalRight = setInterval(() => {
-      if (timerRight === 200 + 1) {
-         clearInterval(intervalRight)
-      } else {
-         changingValuesRight()
-      }
-   }, 100)
+  intervalRight = setInterval(() => {
+    if (timerRight === 200) {
+      clearInterval(intervalRight);
+    } else {
+      changingValuesRight();
+    }
+  }, 100);
 }
 
 function reset() {
-   timerLeft = 200
-   timerRight = 0
-   changingValuesRight()
-   range.value = timerRight
+  timerLeft = 200;
+  timerRight = 0;
+  changingValuesRight();
+  inputRange();
 }
 
-document
-   .querySelector('.c-wrapper__button--item-before')
-   .addEventListener('click', reset)
-document
-   .querySelector('.c-wrapper__button--item-play')
-   .addEventListener('click', start)
-document
-   .querySelector('.c-wrapper__button--item-next')
-   .addEventListener('click', reset)
+document.querySelector(".before").addEventListener("click", reset);
+document.querySelector(".play").addEventListener("click", start);
+document.querySelector(".next").addEventListener("click", reset);
